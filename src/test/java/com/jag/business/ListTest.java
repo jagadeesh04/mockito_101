23e4158512be9355086d06365ce3b4ac378b9a00
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-06 17:31:28 -0400

    Argument Matcher Implemented

---
+++ src/test/java/com/jag/business/ListTest.java
@@ -0,0 +1,42 @@
+package com.jag.business;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class ListTest {
+
+	@Test
+	public void letsMockListSizeMethod() {
+		List listMock = mock(List.class);
+		
+		when(listMock.size()).thenReturn(2);
+		assertEquals(2, listMock.size());
+		assertEquals(2, listMock.size());
+		assertEquals(2, listMock.size());
+	}
+
+	@Test
+	public void letsMockListSize_ReturnMultipleValues() {
+		List listMock = mock(List.class);
+		
+		when(listMock.size()).thenReturn(2).thenReturn(3);
+		assertEquals(2, listMock.size());
+		assertEquals(3, listMock.size());
+	}
+	
+	@Test
+	public void letsMockListGet() {
+		List listMock = mock(List.class);
+		//Argument Matcher
+		when(listMock.get(anyInt())).thenReturn("jag");
+		assertEquals("jag", listMock.get(0));
+		assertEquals("jag", listMock.get(1));
+	}
+	
+}
