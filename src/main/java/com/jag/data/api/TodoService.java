f25ea4e9c6855b14b0a1728cdc24304baf11aade
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-06 22:06:16 -0400

    Verifying Calls on Mock like never, times

--- src/main/java/com/jag/data/api/TodoService.java
+++ src/main/java/com/jag/data/api/TodoService.java
@@ -8,4 +8,5 @@ import java.util.List;
 //Test TodoBusinessImpl using TodoServiceStub
 public interface TodoService {
 	public List<String> retrieveTodos(String user);
+	public void deleteTodo(String todo);
 }
--- src/test/java/com/jag/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/jag/business/TodoBusinessImplMockTest.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
@@ -76,6 +79,28 @@ public class TodoBusinessImplMockTest {
 
 		// Then
 		assertThat(filteredTodos.size(), is(2));
+
+	}
+	
+	@Test
+	public void testDeleteTodosNotRelatedToSpring_BDDWay() {
+
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Mockito", "Learn Latin", "Learn Latin");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+		// When
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
+
+		// Then
+		verify(todoServiceMock, times(2)).deleteTodo("Learn Latin");
+		verify(todoServiceMock).deleteTodo("Learn Mockito");
+		verify(todoServiceMock, never()).deleteTodo("Learn Spring MVC");
 
 	}
 
