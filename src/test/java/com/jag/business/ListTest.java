8fbbec6cd7ca24ddff644fff159df6c6662663c9
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-06 21:54:23 -0400

    BBD for List get method

--- src/test/java/com/jag/business/ListTest.java
+++ src/test/java/com/jag/business/ListTest.java
@@ -1,6 +1,7 @@
 package com.jag.business;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -13,7 +14,7 @@ public class ListTest {
 
 	@Test
 	public void letsMockListSizeMethod() {
-		List listMock = mock(List.class);
+		List<String> listMock = mock(List.class);
 		
 		when(listMock.size()).thenReturn(2);
 		assertEquals(2, listMock.size());
@@ -23,7 +24,7 @@ public class ListTest {
 
 	@Test
 	public void letsMockListSize_ReturnMultipleValues() {
-		List listMock = mock(List.class);
+		List<String> listMock = mock(List.class);
 		
 		when(listMock.size()).thenReturn(2).thenReturn(3);
 		assertEquals(2, listMock.size());
@@ -32,16 +33,30 @@ public class ListTest {
 	
 	@Test
 	public void letsMockListGet() {
-		List listMock = mock(List.class);
+		List<String> listMock = mock(List.class);
 		//Argument Matcher
 		when(listMock.get(anyInt())).thenReturn("jag");
 		assertEquals("jag", listMock.get(0));
 		assertEquals("jag", listMock.get(1));
 	}
 	
+	@Test
+	public void letsMockListGet_UsingBDD() {
+		
+		//Given
+		List<String> listMock = mock(List.class);
+		given(listMock.get(anyInt())).willReturn("jag");
+		
+		//When
+		String firstElement = listMock.get(0);
+		
+		//Then
+		assertEquals("jag", listMock.get(0));
+	}
+	
 	@Test(expected=RuntimeException.class)
 	public void letsMockList_throwAnException() {
-		List listMock = mock(List.class);
+		List<String> listMock = mock(List.class);
 		//Argument Matcher
 		when(listMock.get(anyInt())).thenThrow(new RuntimeException("Something"));
 		listMock.get(0);
