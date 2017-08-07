6fd15427fe76ed7c70356f10c9d0c8d84a2a3647
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-06 23:03:58 -0400

    Hamcrest implementation

--- src/test/java/com/jag/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/jag/business/TodoBusinessImplMockTest.java
@@ -15,6 +15,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import com.jag.data.api.TodoService;
 
@@ -105,6 +106,31 @@ public class TodoBusinessImplMockTest {
 		then(todoServiceMock).should().deleteTodo("Learn Mockito");
 		verify(todoServiceMock, never()).deleteTodo("Learn Spring MVC");
 		then(todoServiceMock).should(never()).deleteTodo("Learn Spring MVC");
+	}
+	
+	@Test
+	public void testDeleteTodosNotRelatedToSpring_BDDWay_captureArgument() {
+		
+		//Declare Argument Captor
+		ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+		//Define Argument Captor on specific method call
+
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Mockito", "Learn Latin", "Learn Latin");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+		// When
+		todoBusinessImpl.deleteTodosNotRelatedToSpring("Dummy");
+
+		// Then
+		then(todoServiceMock).should(times(3)).deleteTodo(stringArgumentCaptor.capture());
+		
+		assertThat(stringArgumentCaptor.getAllValues().size(), is(3));
 	}
 
 }
