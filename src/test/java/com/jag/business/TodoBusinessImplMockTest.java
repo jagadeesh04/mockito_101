e154df13130b2be599d26867613d373d6002d7cc
Jag Gangishetti <jagadeesh.g07@gmail.com>
2017-08-06 21:47:20 -0400

    BDD Example

--- src/test/java/com/jag/business/TodoBusinessImplMockTest.java
+++ src/test/java/com/jag/business/TodoBusinessImplMockTest.java
@@ -1,6 +1,9 @@
 package com.jag.business;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -16,46 +19,64 @@ public class TodoBusinessImplMockTest {
 	@Test
 	public void testRetrieveTodosRelatedToSpring_usingAMock() {
 		TodoService todoServiceMock = mock(TodoService.class);
-		
+
 		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Mockito");
-		
+
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-		
-		
+
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		
+
 		List<String> filteredStub = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		
+
 		assertEquals(2, filteredStub.size());
-	
+
 	}
-	
+
 	@Test
-	public void testRetrieveTodosRelatedToSpring_FirstElement(){
+	public void testRetrieveTodosRelatedToSpring_FirstElement() {
 		TodoService todoServiceMock = mock(TodoService.class);
 		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Mockito");
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-		
-		
+
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		
+
 		List<String> filteredStub = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		
+
 		assertEquals(true, filteredStub.contains("Learn Spring MVC"));
 	}
-	
+
 	@Test
-	public void testRetrieveTodosRelatedToSpring_WithEmptyList(){
+	public void testRetrieveTodosRelatedToSpring_WithEmptyList() {
 		TodoService todoServiceMock = mock(TodoService.class);
 		List<String> todos = Arrays.asList();
 		when(todoServiceMock.retrieveTodos("Dummy")).thenReturn(todos);
-		
-		
+
 		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
-		
+
 		List<String> filteredStub = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
-		
+
 		assertEquals(0, filteredStub.size());
+	}
+
+	// BDD Way
+	@Test
+	public void testRetrieveTodosRelatedToSpring_BDDWay() {
+
+		// Given
+		TodoService todoServiceMock = mock(TodoService.class);
+
+		List<String> todos = Arrays.asList("Learn Spring MVC", "Learn Spring", "Learn Mockito");
+
+		given(todoServiceMock.retrieveTodos("Dummy")).willReturn(todos);
+
+		TodoBusinessImpl todoBusinessImpl = new TodoBusinessImpl(todoServiceMock);
+
+		// When
+		List<String> filteredTodos = todoBusinessImpl.retrieveTodosRelatedToSpring("Dummy");
+
+		// Then
+		assertThat(filteredTodos.size(), is(2));
+
 	}
 
 }
